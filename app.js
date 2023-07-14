ddf45b132eea7d0b90d6d090bcb2fa0659a3add4
iyiy2008 <yusupovismoiljon1204@gmail.com>
2023-07-14 18:58:54 +0500

    header va hero biti

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+window.addEventListener('scroll', ()=>{
+  const elHeader = document.querySelector('header');
+  const elHero = document.querySelector('.hero');
+  const heroCord = elHero.getBoundingClientRect();
+  if (window.screenY > heroCord.top){
+      elHeader.classList.add('sticky');
+  }else{
+      elHeader.classList.remove('sticky');
+  }
+})
 const elmenu = document.getElementById('menu');
 const elmenuOpen = document.getElementById('menu-open');
 const elmenuExit = document.getElementById('menu-exit');
@@ -9,3 +19,21 @@ elmenuOpen.addEventListener('click', () => {
 elmenuExit.addEventListener('click', () => {
     elmenu.classList.toggle('hidden');
 });
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
